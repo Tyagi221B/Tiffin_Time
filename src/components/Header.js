6184525d1ca25f25d5c609fc27b64e97489331fb
Asmit Tyagi <ashmittyagigms@gmail.com>
2023-05-05 12:11:02 +0530

    another commit

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        <img className="main_logo" src="./images/logo.png" alt="my logo img" />
+      </NavLink>
+      <Nav />
+    </MainHeader>
+  );
+};
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  .logo {
+    height: 100px;
+  }
+  .main_logo {
+    height: 100px;
+    width: 200px;
+  }
+`;
+export default Header;
